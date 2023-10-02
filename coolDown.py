6330665d58e502ceb126963d50a8dd4089d23af6
William Souza <william.souza@oogzy.com>
2023-10-02 17:16:03 -0400

    Renamed maxTemp to maxTemperature

--- coolDown.py
+++ coolDown.py
@@ -5,18 +5,18 @@ class CoolDown:
     @classmethod
     def INPUT_TYPES(s):
         return {"required": { "latent": ("LATENT",),
-                              "maxTemp": ("INT", {"default": 70, "min": 50, "max": 90}),
+                              "maxTemperature": ("INT", {"default": 70, "min": 50, "max": 90}),
                               }}
     RETURN_TYPES = ("LATENT",)
     FUNCTION = "CoolDown"
 
     CATEGORY = "utils"
  
-    def CoolDown(self, latent, maxTemp):
+    def CoolDown(self, latent, maxTemperature):
         while True:
             tooHot = False
             for gpu in GPUtil.getGPUs():
-                if gpu.temperature > maxTemp:
+                if gpu.temperature > maxTemperature:
                     tooHot = True
                     print("Waiting... GPU is too hot!", gpu.temperature, "C")
 
